37369a12c00680aff947197cf40546c87fba4299
Thomas Davis <thomasalwyndavis@gmail.com>
2012-08-20 09:06:08 +0000

    site builder

---
+++ site.js
@@ -0,0 +1,22 @@
+var _ = require('underscore');
+var fs = require('fs');
+var marked = require('marked');
+var book = fs.readFileSync('book.txt', 'utf8');
+var indexTemplate = fs.readFileSync('index.template', 'utf8');
+var bookPages = book.split('\n');
+var pages = [];
+_.each(bookPages, function (page) {
+	var pageContents = fs.readFileSync(page, 'utf8');
+	var title = marked.lexer(pageContents)[0].text;
+	var link = page.replace('.md', '.html');
+	pages.push({
+		title: title,
+		contents: marked(pageContents),
+		link: link
+	})
+});
+_.each(pages, function(page){
+	page.contents = page.contents.replace(/table>/g, 'table class="table">');
+	var currentPage = _.template(indexTemplate, {pages: pages, currentPage: page.contents});
+	fs.writeFileSync(page.link, currentPage, 'utf8');
+});
